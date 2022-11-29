25279ad9dc5f04ad8664013d3700d8999af17b81
Ignacio Planas <80254262+IgnacioPlanasTh@users.noreply.github.com>
2022-11-29 12:24:50 +0100

    final commit

--- src/oscar/apps/home/views.py
+++ src/oscar/apps/home/views.py
@@ -1,12 +1,8 @@
-from urllib.parse import quote
-
 from django.contrib import messages
 from django.core.paginator import InvalidPage
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
-
-from oscar.apps.catalogue.signals import product_viewed
 from oscar.core.loading import get_class, get_model
 
 get_product_search_handler_class = get_class(
@@ -15,23 +11,42 @@ get_product_search_handler_class = get_class(
 
 from django.views.generic import ListView
 from oscar.core.loading import get_model
-ConditionalOffer = get_model('offer', 'ConditionalOffer')
+Product = get_model('catalogue',"Product")
+
+
 
 class HomeView(ListView):
-    model = ConditionalOffer
-    context_object_name = 'offers'
+    model = Product
+    """
+    Browse all products in the catalogue
+    """
+    context_object_name = "products"
     template_name = 'oscar/home/home.html'
 
+    def get(self, request, *args, **kwargs):
+        try:
+            self.search_handler = self.get_search_handler(
+                self.request.GET, request.get_full_path(), [])
+            response = super().get(request, *args, **kwargs)
+        except InvalidPage:
+            # Redirect to page one.
+            messages.error(request, _('The given page number was invalid.'))
+            return redirect('catalogue:index')
+        return response
+
     def get_queryset(self):
-        """
-        Return a queryset of active :py:class:`ConditionalOffer <oscar.apps.offer.abstract_models.AbstractConditionalOffer>`
-        instances with an :py:attr:`offer_type <oscar.apps.offer.abstract_models.AbstractConditionalOffer.offer_type>`
-        of :py:const:`ConditionalOffer.SITE <oscar.apps.offer.abstract_models.AbstractConditionalOffer.SITE>`.
-        """  # noqa
-        return ConditionalOffer.active.filter(
-            offer_type=ConditionalOffer.SITE)
+        return Product.objects
 
+    def get_search_handler(self, *args, **kwargs):
+        return get_product_search_handler_class()(*args, **kwargs)
 
+    def get_context_data(self, **kwargs):
+        ctx = {}
+        ctx['summary'] = _("All products")
+        search_context = self.search_handler.get_search_context_data(
+            self.context_object_name)
+        ctx.update(search_context)
+        return ctx
 
 
 
