bea7263f350abbf2dff987cc61faa173f59856f5
Ignacio Planas <80254262+IgnacioPlanasTh@users.noreply.github.com>
2022-12-10 22:00:55 +0100

    Envio de correos electronicos

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -13,7 +13,15 @@ DEBUG = env.bool('DEBUG', default=True)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 EMAIL_SUBJECT_PREFIX = '[BIA] '
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+''' EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' '''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST     = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'qlluozbcivkszttb'
+EMAIL_HOST_USER = "labibliotecaignifuga@gmail.com"
+EMAIL_PORT      = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 
 # Use a Sqlite database by default
 DATABASES = {
